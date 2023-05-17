ee4b3b792d6932cfe54afa7f9aa7677f34f7d8d3
nathaliacosim <nathalia.cosim@novaandradina.org>
2023-05-17 17:36:16 -0400

    Adicionar arquivos de projeto.

---
+++ CollateCsharp/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<List<int>> vector = Arrayzada.Collate(new List<int> { 1, 2, 3, 4, 5 }, 3);
+                
+        Console.Write("[");
+        foreach (List<int> a in vector)
+        {
+            Console.Write("[");
+            foreach(int b in a)
+            {
+                Console.Write(b+", ");
+            }
+            Console.Write("],");
+        }
+        Console.Write("]");
+    }
+}
+
+public class Arrayzada {
+    public static IEnumerable<List<T>> Collate<T>(List<T> locations, int nSize = 30)
+    {
+        for (int i = 0; i < locations.Count; i += nSize)
+        {
+            yield return locations.GetRange(i, Math.Min(nSize, locations.Count - i));
+        }
+    }
+}
